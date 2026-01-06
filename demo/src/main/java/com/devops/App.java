21c0ab65cab4217480e55675e10c1c1a99904cd4
Mohamedaithammou <maithammou5@gmail.com>
2026-01-06 14:14:33 +0100

    Update message on dev branch

--- demo/src/main/java/com/devops/App.java
+++ demo/src/main/java/com/devops/App.java
@@ -2,6 +2,7 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour et bon courage dans votre projet DevOps");
+        System.out.println("Bonjour et bon courage dans votre projet DevOps - branche dev");
+
     }
 }
